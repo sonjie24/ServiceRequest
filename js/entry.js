7fb92a298432a940368dad23b3abb081b381c687
Sonjie <your@email.com>
2025-07-21 18:02:28 +0800

    update

--- js/entry.js
+++ js/entry.js
@@ -1,17 +1,15 @@
 (() => {
-
   //const data = [{"Treasury":{"Business Tax":[{"name":"Formn1","code":"FRM000001"},{"name":"Formn2","code":"FRM000002"},{"name":"Formn3","code":"FRM000003"},{"name":"Formn4","code":"FRM000004"}],"Real Property Tax":[{"name":"Formn5","code":"FRM000005"},{"name":"Formn6","code":"FRM000006"},{"name":"Formn7","code":"FRM000007"},{"name":"Formn8","code":"FRM000008"},{"name":"Formn9","code":"FRM000009"},{"name":"Formn10","code":"FRM000010"}]}}];
   const data = JSON.parse(sessionStorage.getItem("functionalities"));
 
-  
   const systemSelect = document.getElementById("systemSelect");
   const moduleSelect = document.getElementById("moduleSelect");
   const formSelect = document.getElementById("formSelect");
   const formNoInput = document.getElementById("formNo");
 
   const systems = data.Functionalities;
-  
-  Object.keys(systems).forEach(system => {
+
+  Object.keys(systems).forEach((system) => {
     const option = document.createElement("option");
     option.value = system;
     option.textContent = system;
@@ -20,13 +18,15 @@
 
   systemSelect.addEventListener("change", () => {
     const selectedSystem = systemSelect.value;
-    moduleSelect.innerHTML = '<option value="" disabled selected>Select Module</option>';
-    formSelect.innerHTML = '<option value="" disabled selected>Select Form</option>';
+    moduleSelect.innerHTML =
+      '<option value="" disabled selected>Select Module</option>';
+    formSelect.innerHTML =
+      '<option value="" disabled selected>Select Form</option>';
     formSelect.disabled = true;
 
     if (selectedSystem && systems[selectedSystem]) {
       moduleSelect.disabled = false;
-      Object.keys(systems[selectedSystem]).forEach(module => {
+      Object.keys(systems[selectedSystem]).forEach((module) => {
         const option = document.createElement("option");
         option.value = module;
         option.textContent = module;
@@ -42,11 +42,16 @@
     const selectedSystem = systemSelect.value;
     const selectedModule = moduleSelect.value;
 
-    formSelect.innerHTML = '<option value="" disabled selected>Select Form</option>';
+    formSelect.innerHTML =
+      '<option value="" disabled selected>Select Form</option>';
 
-    if (selectedSystem && selectedModule && systems[selectedSystem][selectedModule]) {
+    if (
+      selectedSystem &&
+      selectedModule &&
+      systems[selectedSystem][selectedModule]
+    ) {
       formSelect.disabled = false;
-      systems[selectedSystem][selectedModule].forEach(form => {
+      systems[selectedSystem][selectedModule].forEach((form) => {
         const option = document.createElement("option");
         option.value = form.code;
         option.textContent = `${form.name}`;
@@ -64,7 +69,9 @@
   });
 
   window.addRow = function () {
-    const table = document.getElementById("dataTable").getElementsByTagName('tbody')[0];
+    const table = document
+      .getElementById("dataTable")
+      .getElementsByTagName("tbody")[0];
     const newRow = document.createElement("tr");
 
     newRow.innerHTML = `
