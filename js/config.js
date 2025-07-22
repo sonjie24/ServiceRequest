45e4a271684231285092c937f5a1d49f981234e1
sonjie24 <55346939+sonjie24@users.noreply.github.com>
2025-07-22 23:09:23 +0800

    update

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ const API_BASE_URL = "https://f33f9373d24d.ngrok-free.app/webhook";
 // Dynamically set the <base> tag for GitHub Pages vs local
 // config.js
 (function () {
-    const repoName = "ServiceRequest-main"; // Update if needed
+    const repoName = "ServiceRequest"; // Update if needed
     const isGithub = window.location.hostname.includes("github.io");
   
     const basePath = isGithub ? `/${repoName}/` : '/';
