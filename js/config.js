fa44f47b2b0632dd1c3f146fe02af9e90738a2b7
ITDQPORRAS <42963615+ITDQPORRAS@users.noreply.github.com>
2025-07-23 17:28:35 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "http://localhost:5678/webhook";
-const API_BASE_URL = "https://f33f9373d24d.ngrok-free.app/webhook";
+const API_BASE_URL = "https://500e18becc4f.ngrok-free.app/webhook";
 
  
 // Dynamically set the <base> tag for GitHub Pages vs local
@@ -8,7 +8,7 @@ const API_BASE_URL = "https://f33f9373d24d.ngrok-free.app/webhook";
     const repoName = "ServiceRequest"; // Update if needed
     const isGithub = window.location.hostname.includes("github.io");
   
-    const basePath = isGithub ? `/${repoName}/` : '/';
+    const basePath = isGithub ? `/${repoName}/` : `/${repoName}/`;
   
     const base = document.createElement("base");
     base.href = basePath;
