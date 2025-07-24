9de7d4f53392f665b0d25005d6912216934f3263
sonjie24 <55346939+sonjie24@users.noreply.github.com>
2025-07-24 18:21:50 +0800

    update

--- js/config.js
+++ js/config.js
@@ -1,5 +1,13 @@
-//const API_BASE_URL = "http://localhost:5678/webhook";
-const API_BASE_URL = "https://500e18becc4f.ngrok-free.app/webhook";
+
+
+const hostname = window.location.hostname;
+const isLocal = hostname === "20.20.40.221";
+
+// Use a local or remote API base URL depending on environment
+const API_BASE_URL = isLocal
+  ? "http://20.20.40.221:5679/webhook"               // Local dev server
+  : "https://500e18becc4f.ngrok-free.app/webhook"; // Production API
+
 
  
 // Dynamically set the <base> tag for GitHub Pages vs local
@@ -7,7 +15,6 @@ const API_BASE_URL = "https://500e18becc4f.ngrok-free.app/webhook";
 (function () {
     const repoName = "ServiceRequest"; // Update if needed
     const isGithub = window.location.hostname.includes("github.io");
-  
     const basePath = isGithub ? `/${repoName}/` : '/';
   
     const base = document.createElement("base");
