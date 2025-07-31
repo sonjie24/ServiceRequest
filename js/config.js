9e9ad2e7609088f85aa1b17e30135f043ea0866a
Sonjie <your@email.com>
2025-07-31 10:31:01 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,14 +22,12 @@
   // Set API base URL based on hostname
   let apiBaseUrl = "";
 
-  // if (hostname.includes("20.20.40.221")) {
-  //   apiBaseUrl = "http://20.20.40.221:5678/webhook"; // Fallback
-  // } else {
-  //   // GitHub Pages can't make backend calls directly unless it's public API or proxied
-  //   apiBaseUrl = "https://500e18becc4f.ngrok-free.app/webhook"; // Replace with real prod endpoint
-  // }
-
-   apiBaseUrl = "http://20.20.40.221:5678/webhook"; // Fallback
+  if (hostname.includes("20.20.40.221")) {
+    apiBaseUrl = "http://20.20.40.221:5678/webhook"; // Fallback
+  } else {
+    // GitHub Pages can't make backend calls directly unless it's public API or proxied
+    apiBaseUrl = "https://500e18becc4f.ngrok-free.app/webhook"; // Replace with real prod endpoint
+  }
 
   // Expose API_BASE_URL to other scripts
   window.API_BASE_URL = apiBaseUrl;
